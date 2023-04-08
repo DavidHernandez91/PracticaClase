164dce7cc97b290a3721bc49dc747b644b4fee5f
Hernandez David <acevedodavid15@gmail.com>
2023-04-08 17:29:20 -0400

    Actualizado con bloqueo

--- SocialTravel/views.py
+++ SocialTravel/views.py
@@ -4,7 +4,8 @@ from SocialTravel.models import Post
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 
 def index(request):
     return render(request,"SocialTravel/index.html")
@@ -23,14 +24,30 @@ class PostCreate(LoginRequiredMixin, CreateView):
     fields = '__all__' #o si quiere usarse algunos campos , se usa lista con los nombres de los campos 
     # ['carousel_caption_tittle','carousel_caption_description']
 
-class PostUpdate(LoginRequiredMixin,UpdateView):
+class PostUpdate(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     model = Post
     success_url = reverse_lazy("post-list")
     fields = '__all__'
 
-class PostDelete(LoginRequiredMixin,DeleteView):
+    def test_func(self):
+        user_id = self.request.user.id
+        post_id = self.kwargs.get('pk')
+        return Post.objects.filter(publisher=user_id, id=post_id).exists()
+
+    def handle_no_permission(self):
+        return render(self.request,"SocialTravel/not_found.html")
+
+class PostDelete(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
     model = Post
     success_url = reverse_lazy("post-list")
+
+    def test_func(self):
+        user_id = self.request.user.id
+        post_id = self.kwargs.get('pk')
+        return Post.objects.filter(publisher=user_id, id=post_id).exists()
+
+    def handle_no_permission(self):
+        return render(self.request,"SocialTravel/not_found.html")
 
 class SignUp(CreateView):
     form_class = UserCreationForm
